76bc3cdf35f3840a86683548e1c5cc7b1205e704
Jeff Standen <jeff@webgroupmedia.com>
2012-01-16 15:49:53 -0800

    Changed cURL options to not verify SSL peers since new versions don't bundle CA certs.

--- api/App.php
+++ api/App.php
@@ -99,9 +99,10 @@ class WgmClickatell_API {
 		
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		$out = curl_exec($ch);
 		curl_close($ch);
 		
